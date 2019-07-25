7f796e42afb55e35727ab5a08da715412d6468a1
Nick Veys <nick@elixir.blue>
2019-07-25 15:34:04 -0500

    Section 1-15 changes

--- 1-operators/src/app/about/about.component.ts
+++ 1-operators/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { of, concat } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,11 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+    const result$ = concat(source1$, source2$, source3$);
 
+    result$.subscribe(console.log);
   }
 }
