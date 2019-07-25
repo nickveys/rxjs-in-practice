24752c7203e35d8a43fac987d9164c0db30e297a
Nick Veys <nick@elixir.blue>
2019-07-25 15:57:06 -0500

    Section 1-18 changes

--- 1-operators/src/app/about/about.component.ts
+++ 1-operators/src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { of, concat } from 'rxjs';
+import { of, concat, interval, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -11,11 +12,9 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const source1$ = of(1, 2, 3);
-    const source2$ = of(4, 5, 6);
-    const source3$ = of(7, 8, 9);
-    const result$ = concat(source1$, source2$, source3$);
-
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(map(val => val * 10));
+    const result$ = merge(interval1$, interval2$);
     result$.subscribe(console.log);
   }
 }
